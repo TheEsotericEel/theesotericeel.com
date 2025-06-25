83c8d72154ade936c82319eb3f9902f9be87eebc
TheEsotericEel <The.esoteric.eel@gmail.com>
2025-06-25 17:04:35 -0600

    layout.tsx test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
+import { ClerkProvider, SignIn } from "@clerk/nextjs";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <div style={{ color: 'black', background: 'white', padding: '2rem' }}>
-          Hello, world! (outside Clerk)
-        </div>
-        {children}
+        <ClerkProvider>
+          <div style={{ color: 'black', background: 'white', padding: '2rem' }}>
+            Hello from ClerkProvider!
+          </div>
+          <SignIn />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
