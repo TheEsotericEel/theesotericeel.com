db3054d1d06cc03948ce69cf779871dbfc8d3fe1
TheEsotericEel <The.esoteric.eel@gmail.com>
2025-06-25 17:07:01 -0600

    clerk portal test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import { ClerkProvider, SignIn } from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <ClerkProvider>
-          <div style={{ color: 'black', background: 'white', padding: '2rem' }}>
-            Hello from ClerkProvider!
-          </div>
-          <SignIn />
-          {children}
+          <SignedIn>
+            {children}
+          </SignedIn>
+          <SignedOut>
+            <SignIn />
+          </SignedOut>
         </ClerkProvider>
       </body>
     </html>
